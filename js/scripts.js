be35903e582be2e66d4956df222edc60f2c9e82f
Logan Wu <student@epicodus.com>
2015-09-18 14:25:00 -0700

    add showing and emptying list of toppings function

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,15 @@ PizzaOrder.prototype.totalCost = function() {
     return totalCost;
 }
 
+//find the checked topping boxes
+function findToppings() {
+    var toppingsArray = [];
+    $('input[type="checkbox"]:checked').each(function() {
+        toppingsArray.push($(this).val());
+    });
+    return toppingsArray;
+}
+
 function resetFields() {
     //empty checked boxes after each submission
     $("#checkbox input:checkbox").removeAttr('checked');
@@ -25,28 +34,32 @@ function resetFields() {
 }
 
 $(document).ready(function() {
+    //does not empty the list after new submission
+    $("ul#toppings").val("");
+
     $("form#new-order").submit(function(event) {
         event.preventDefault();
 
+        //get all inputs from form submission and create new object called newOrder
         var inputtedType = $("select#new-type").val();
         var inputtedToppings = $('#checkbox :checkbox:checked').length;
-        // console.log($('#checkbox :checkbox:checked').length);
-        // console.log(inputtedToppings);
         var inputtedSize = $("select#new-size").val();
         var inputtedQuantity = parseInt($("input#new-quantity").val());
-
         var newOrder = new PizzaOrder(inputtedType, inputtedToppings, inputtedSize, inputtedQuantity);
 
+        //calculate total cost
         var totalCost = newOrder.totalCost();
 
-        console.log(totalCost);
-
+        //show all the inputs and calculated total cost
         $("#show-cost").show();
         $("span#totalCost").text(totalCost);
         $("span#pizzaType").text(inputtedType);
-        // $("#checkbox").forEach(function(topping) {
-        //     $("ul#toppings").append("<li>" + topping + "</li>");
-        // });
+
+        var toppingsArray = findToppings();
+        toppingsArray.forEach(function(topping) {
+            $("ul#toppings").append("<li>" + topping + "</li>");
+        });
+
         $("span#size").text(inputtedSize);
         $("span#quantity").text(inputtedQuantity);
 
